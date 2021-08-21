64d996692c8c5b56d0b78fab3fb41cc3e45c8e18
goguma9071 <whjhkkhjl@gmail.com>
2021-08-22 03:23:18 +0900

    1

---
+++ src/game.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class game {
+
+    static class MyFrame extends JFrame {
+
+        //constant
+
+        //variable
+
+        public MyFrame (String title) {
+            super(title);
+        }
+    }
+    public static void main(String[] args) {
+        new MyFrame("gamme");
+    }
+}
